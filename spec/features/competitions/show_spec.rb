c317b91dbeb12db28d9d5a9403e948c61d1d51ac
Aurora Sanderson <AuroraTuring@gmail.com>
2024-05-13 17:59:22 -0600

    Complete user story seven

--- spec/features/competitions/show_spec.rb
+++ spec/features/competitions/show_spec.rb
@@ -14,6 +14,19 @@ RSpec.describe "competitions show page", type: :feature do
 
         expect(page).to have_content(new_england.location)
       end
+
+        # As a visitor
+        # When I visit a parent's show page
+        # I see a count of the number of children associated with this parent
+      it "I see a count of the number of children associated with this parent" do
+        new_england = Competition.create!(name: "New England Show", location: "Ohio", date: "04/01/2024", open_to_the_public: true, prize_money: 250)
+        Gerbil.create!(competition_id: new_england.id, name: "Crystal", color: "Mottled Lilac", age: 2.5, neutered: false)
+        Gerbil.create!(competition_id: new_england.id, name: "Jasper", color: "Polar Fox", age: 3, neutered: false)
+
+        visit "/competitions/#{new_england.id}"
+
+        expect(page).to have_content("2 gerbils")
+      end
     end
   end
 end
